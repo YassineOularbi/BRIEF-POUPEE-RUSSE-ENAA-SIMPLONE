df1fd1ebd7b9c85d44092c0c10406ab1152bccc1
YassineOularbi <yassineoularbi4@gmail.com>
2024-02-15 17:20:27 +0100

    Création de la classe PoupéeRusse (avec des méthodes abstraction) rencontre des erreurs

--- Poupee.java
+++ Poupee.java
@@ -1,18 +1,10 @@
 public abstract class Poupee {
-    private String Name;
     private int Taille;
-    private Boolean isOpen;
-    public Poupee (String name, int taille, Boolean isopen){
-            this.Name = name;
+    public Poupee (int taille){
             this.Taille = taille;
-            this.isOpen = isopen;
     }
-    public String getName(){
-        return Name;
-    }
-    public void setName(String name){
-        this.Name = name;
-    }
+    public Poupee(){}
+
     public int getTaille(){
         return Taille;
     }
@@ -20,12 +12,6 @@ public abstract class Poupee {
         this.Taille = taille;
     }
 
-    public Boolean getOpen() {
-        return isOpen;
-    }
-    public void setOpen(Boolean isopen){
-        this.isOpen = isopen;
-    }
     public abstract void Open();
     public abstract void Close();
     public abstract void PlaceInto(Poupee p);
--- PoupeeRusse.java
+++ PoupeeRusse.java
@@ -1,3 +1,82 @@
-class PoupeeRusse extends Poupee {
+import java.util.Scanner;
 
+public class PoupeeRusse extends Poupee {
+    public PoupeeRusse(){
+        super();
+    }
+    private String Name;
+    private String Color;
+    private Boolean isOpen;
+    public PoupeeRusse(String SuperName, String SuperColor, int SuperTaille, Boolean SuperisOpen){
+        super(SuperTaille);
+        this.Name = SuperName;
+        this.Color = SuperColor;
+        this.isOpen = SuperisOpen;
+    }
+       public String getName(){
+        return Name;
+    }
+    public void setName(String name){
+        this.Name = name;
+    }
+    public String getColor(){
+        return Color;
+    }
+    public void setColor(String color){
+        this.Color = color;
+    }
+    @Override
+    public int getTaille() {
+        return super.getTaille();
+    }
+
+    @Override
+    public void setTaille(int taille) {
+        super.setTaille(taille);
+    }
+    public Boolean getIsOpen(){
+        return isOpen;
+    }
+    public void setIsOpen(Boolean isopen){
+        this.isOpen = isopen;
+    }
+
+    @Override
+    public void Open() {
+
+    }
+
+    @Override
+    public void Close() {
+
+    }
+
+    @Override
+    public void PlaceInto(Poupee p) {
+
+    }
+
+    @Override
+    public void Leave(Poupee p) {
+
+    }
+    public void AddRussianDoll(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entrez le nom de la première poupée : ");
+        String Name = scanner.nextLine();
+        setName(Name);
+        System.out.println("Entrez la couleur de la première poupée : ");
+        String Color = scanner.nextLine();
+        setColor(Color);
+        System.out.println("Entrez la taille de la première poupée : ");
+        int Taille = scanner.nextInt();
+        setTaille(Taille);
+        System.out.println("Entrez l'etat de la poupée : ");
+        Boolean isOpen = scanner.nextBoolean();
+        setIsOpen(isOpen);
+    }
+
+    public void Display(){
+        System.out.println("Name : "+getName()+" Color : " +getColor()+"Taille : "+getTaille()+" ouverte : "+getIsOpen());
+    }
 }
